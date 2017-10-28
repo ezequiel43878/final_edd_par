17db13c0f7ec1e07431094bbb03b03408ec58d12
ezequiel <ezequiel43878@gmail.com>
2017-10-28 20:58:54 -0300

    Login terminado

--- app_farm.py
+++ app_farm.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import csv
+from flask import Flask, render_template, flash, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from forms import LoginForm
 
@@ -7,14 +8,41 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'key_secret'
 
-@app.route("/")
+@app.route("/")                 #Pagina de inicio
 def index():
 	return render_template("index.html") 
 
-@app.route("/login")
+
+@app.route("/login", methods = ['GET','POST'])          #Login de usuario 
 def login():
 	login = LoginForm()
+	if login.validate_on_submit():							#Recibe los datos y crea session en caso de ser correcto Y redireccion a ultimas_ventas
+		with open("csv/usuarios.csv","r") as archivo:
+			usuarios = csv.reader(archivo)
+			for x in usuarios:
+				if login.usuario.data == x[0] and login.password.data == x[1]:
+					session['usuario'] = login.usuario.data
+					return redirect (url_for("ultimas_ventas"))
+			else: 
+				flash("Datos Incorrectos")
+				return redirect (url_for("login"))
 	return render_template("login.html", formulario = login)
+
+
+@app.route("/ultimas_ventas", methods=['GET','POST'])     # pagina de ultimas_ventas en caso de sesion iniciada sino redirecciono a indexx 
+def ultimas_ventas():
+	if 'usuario' in session:
+		user = session['usuario']
+		edad = 17
+		return render_template("ultimas_ventas.html", usuario = user , edad = edad)
+	else:
+		return redirect (url_for("index"))
+
+@app.route("/logout")           # Destruyo la session y redirecciono a index 
+def logout():
+	if 'usuario' in session:
+		session.pop('usuario')
+	return redirect (url_for("index"))	
 
 if __name__ == ("__main__"):
 	app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required
 
 
 
 class LoginForm(FlaskForm):
-	usuario = StringField('Usuario:')
-	contraseña = PasswordField('Contraseña:')
+	usuario = StringField('Usuario:', validators=[Required()])
+	password = PasswordField('Contraseña:',validators=[Required()])
 	enviar = SubmitField('Enviar')
